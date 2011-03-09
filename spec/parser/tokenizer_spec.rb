464385ec346a1cfe0d6008b77a304531687c4181
lkdjiin <xavier.nayrac@gmail.com>
2011-03-09 16:25:19 +0100

    added missing spec for Tokenizer

---
+++ spec/parser/tokenizer_spec.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+require './spec/helper'
+
+
+describe Tokenizer do
+
+  it "must produce tokens" do
+    tokenizer = Tokenizer.new "new Object"
+    tokenizer.has_more_token?.should == true
+    2.times {tokenizer.next_token}
+    tokenizer.has_more_token?.should == false
+  end
+  
+  it "must produce name-like token" do
+    tokenizer = Tokenizer.new "a-thing A-Thing"
+    tokenizer.next_token.should == "a-thing"
+    tokenizer.next_token.should == "A-Thing"
+  end
+  
+  it "must produce integer-like token" do
+    tokenizer = Tokenizer.new "1234"
+    tokenizer.next_token.should == 1234
+  end
+  
+end
